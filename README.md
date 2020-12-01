59382c77d4d8e03484a098dfe85aa7965e4448ff
Timothy Whittaker <timothy.whittaker@datarobot.com>
2020-11-30 22:17:16 -0500

    add push.py

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 Ford MLOps Showcase
 
+# Requirements
+
+`datarobot==2.23.0b0`
+`datarobot-drum>=1.4.4`
+
 # Fit a model using drum
 
-drum fit --code-dir ./training-code --input ./data/loss_cost_demo.csv --output ./model --target-type regression --target IncurredClaims  --docker env --verbose
+`drum fit --code-dir ./training-code --input ./data/loss_cost_demo.csv --output ./model --target-type regression --target IncurredClaims  --docker env --verbose`
 
 # Test model
 
-drum perf-test --code-dir ./model --input ./data/loss_cost_demo_inference.csv --target-type regression --docker env
-
+`drum perf-test --code-dir ./model --input ./data/loss_cost_demo_inference.csv --target-type regression --docker env`
 
 # Validate Model
 
-drum validation --code-dir ./model --input ./data/loss_cost_demo_inference.csv --target-type regression --docker env
+`drum validation --code-dir ./model --input ./data/loss_cost_demo_inference.csv --target-type regression --docker env`
 
+# Push Execution Environment
+
+`python push.py --env-dir ./env --model-dir ./model`
+
+# Push Custom Inference Model
+
+`drum push --code-dir ./model --verbose --logging-level info`
 
--- push.py
+++ push.py
@@ -7,8 +7,8 @@ import logging
 parser = argparse.ArgumentParser(description="execution environment push")
 # parser.add_argument('integers', metavar='N', type=int, nargs='+',
 #                     help='an integer for the accumulator')
-parser.add_argument("--execution-env-path", default=None)
-parser.add_argement("--model-dri", default=None)
+parser.add_argument("--env-dir", default=None)
+parser.add_argument("--model-dir", default=None)
 parser.add_argument("--logging-level", default="INFO")
 parser.add_argument("--max-wait", default=None)
 
@@ -20,34 +20,50 @@ logger = logging.getLogger("drum push")
 def main(path, model_dir, max_wait): 
     client = dr.Client(os.environ["DATAROBOT_API_TOKEN"], os.environ["DATAROBOT_ENDPOINT"])
     try:
-        with open( os.path.join(model_dir,"model-metadata.yaml"), "r") as f:
-            model_metadata = yaml.load(f, Loader=yaml.FullLoader)
-        logger.info(model_metadata)
-        env_id = model_metadata.get("environmentID")
-    except:
-        model_metadata = None
-        env_id = None
+        with open( os.path.join(path, "env-info.yaml"), "r") as f:
+            env_config = yaml.load(f, Loader=yaml.FullLoader)
+        logger.info(env_config)
+        env_id = env_config.get("id")
+        env_desc = env_config.get("description")
+        env_name = env_config.get("name")
+        env_language = env_config.get("programmingLanguage")
+        version_label = env_config.get("versionLabel")
+        version_desc = env_config.get("versionDescription")
+    except Exception as e:
+        logger.error(e)
+        logger.error("no environment info yaml found")
     if env_id is None:
+        logger.info("creating execution environment")
         env = dr.ExecutionEnvironment.create(
-            name = "Execution Environment",
-            description = "some description",
-            programming_language= "python")
+            name = env_name,
+            description = env_desc,
+            programming_language= env_language)
+        env_config["id"] = env.id
     else:
         env = dr.ExecutionEnvironment.get(env_id)
-    dr.ExecutionEnvironmentVersion.create(
+    logger.info("creating new execution environment version")
+    version = dr.ExecutionEnvironmentVersion.create(
         execution_environment_id = env.id,
-        docker_context_path = "./env",
-        label = "version 1",
-        description = "initializing the execution environment", 
+        docker_context_path = path,
+        label = version_label,
+        description = version_desc, 
         max_wait = max_wait
     )
-    if model_metadata is not None:
-        model_metadata["environmentID"] = env.id
-        with open(os.path.join(code_dir,"model-metadata.yaml"), "w") as f:
-            yaml.dump(model_metadata, f)
+    env_config["environmentVersionID"] = version.id
+    if model_dir is not None:
+        with open( os.path.join(model_dir,"model-metadata.yaml"), "r") as f:
+            model_config = yaml.load(f, Loader=yaml.FullLoader)
+        logger.info("update model metadata yaml")
+        model_config["environmentID"] = env.id
+        with open(os.path.join(model_dir,"model-metadata.yaml"), "w") as f:
+            yaml.dump(model_config, f)
+    logger.info("update env info yaml")
+    with open(os.path.join(path,"env-info.yaml"), "w") as f:
+            yaml.dump(env_config, f)
  
-if __name__ == "main":
+if __name__ == "__main__":
     args = parser.parse_args()
+    print(args)
     logger.setLevel(args.logging_level)
     logger.info(args)
-    main(args.execution_env_path, args.model_dir, args.max_wait)
+    main(args.env_dir, args.model_dir, args.max_wait)
