176b91ec9ea83334041e9e5d5038732b2ea320db
Timothy Whittaker <timothy.whittaker@datarobot.com>
2020-11-30 20:08:18 -0500

    Update custom.py

--- training-code/custom.py
+++ training-code/custom.py
@@ -3,8 +3,13 @@ import pandas as pd
 import numpy as np
 import yaml
 from typing import List, Optional, Any, Dict
-from create_pipeline import make_regressor
 import logging
+import os
+
+def init(code_dir, **kwargs):
+    logging.info("init call, code_dir -> {}".format(code_dir))
+    os.environ["code_dir"] = code_dir
+    logging.info("init call, kwargs -> {}".format(kwargs))
 
 def fit(
     X: pd.DataFrame,
@@ -42,9 +47,10 @@ def fit(
     -------
     Nothing
     """
+    from create_pipeline import make_regressor
     offset = np.log(X["Exposure"].values)
     estimator = make_regressor()
-    estimator.fit(X, y, model__init_score=offset)
+    estimator.fit(X, y, model__init_score = offset)
     with open("{}/artifact.pkl".format(output_dir), "wb") as fp:
         pickle.dump(estimator, fp)
 
@@ -55,6 +61,6 @@ def score(data: pd.DataFrame, model: Any, **kwargs: Dict[str, Any]) -> pd.DataFr
     If your model is for classification, you likely want to ensure this function
     calls `predict_proba()`, whereas for regression it should use `predict()`
     """
-    offset = data["Exposure"]
-    predictions = np.exp(model.predict(data, model__raw_score=True)) * offset
+    offset = data["Exposure"].values
+    predictions = np.exp(model.predict(data, raw_score=True)) * offset
     return pd.DataFrame(predictions, columns = ["Predictions"])
