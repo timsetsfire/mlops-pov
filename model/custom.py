d51bad218b860265557b22360246d1bda10924bb
Timothy Whittaker <timsetsfire@gmail.com>
2021-08-16 09:42:24 -0400

    Update custom.py

--- model/custom.py
+++ model/custom.py
@@ -85,24 +85,24 @@ def transform(data, model):
     data = model.preprocess(data)
     return data
 
-def score_unstructured(model, data, query, **kwargs):
-    print("Incoming content type params: ", kwargs)
-    print("Incoming data type: ", type(data))
-    print("Incoming query params: ", query)
+# def score_unstructured(model, data, query, **kwargs):
+#     print("Incoming content type params: ", kwargs)
+#     print("Incoming data type: ", type(data))
+#     print("Incoming query params: ", query)
 
-    X = pd.read_csv(BytesIO(data))
-    X = transform(X, model)
-    with open(os.path.join(model.code_dir, "feature_detail.yaml"), "r") as f:
-        feature_type_dict = yaml.load(f, Loader=yaml.FullLoader)
-    numeric_features = feature_type_dict["Numeric"]
-    categorical_features = feature_type_dict["Categorical"]
-    c = numeric_features + categorical_features
-    Xs = model.transformer.transform(X)
-    shap_values = model.shap_explainer.shap_values(Xs)[1]
-    pos_class_shap = pd.DataFrame(shap_values, columns = [f"POS_CLASS_SHAP_{col}" for col in c]).to_dict(orient="records")
-    predictions = pd.DataFrame(model.predict(X), columns = ["negative_class_label_prob", "positive_class_label_prob"]).to_dict(orient="records")
-    out = {"data": []}
-    for p, s in zip(predictions, pos_class_shap):
-        temp_dict = {"predictions": p, "shap_values": s}
-        out["data"].append(temp_dict)
-    return json.dumps(out)
+#     X = pd.read_csv(BytesIO(data))
+#     X = transform(X, model)
+#     with open(os.path.join(model.code_dir, "feature_detail.yaml"), "r") as f:
+#         feature_type_dict = yaml.load(f, Loader=yaml.FullLoader)
+#     numeric_features = feature_type_dict["Numeric"]
+#     categorical_features = feature_type_dict["Categorical"]
+#     c = numeric_features + categorical_features
+#     Xs = model.transformer.transform(X)
+#     shap_values = model.shap_explainer.shap_values(Xs)[1]
+#     pos_class_shap = pd.DataFrame(shap_values, columns = [f"POS_CLASS_SHAP_{col}" for col in c]).to_dict(orient="records")
+#     predictions = pd.DataFrame(model.predict(X), columns = ["negative_class_label_prob", "positive_class_label_prob"]).to_dict(orient="records")
+#     out = {"data": []}
+#     for p, s in zip(predictions, pos_class_shap):
+#         temp_dict = {"predictions": p, "shap_values": s}
+#         out["data"].append(temp_dict)
+#     return json.dumps(out)
